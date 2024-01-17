5520934ac154c29562e65ceaf59b942a28aba4d5
eren yeager <gdutched@gmail.com>
2024-01-17 10:21:49 +0100

    car script

---
+++ Assets/scripts/CarController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CarController : MonoBehaviour
+{
+    public enum Axel
+    {
+        Front,
+        Rear
+    }
+    
+    [Serializable]
+    public struct Wheel
+    {
+        public GameObject wheelModel;
+        public WheelCollider wheelCollider;
+        public Axel axel;
+    }
+    public float maxAcceleration = 90.0f;
+    public float breakAcceleration = 50.0f;
+
+    public List<Wheel> wheels;
+
+    float moveInput;
+
+    private Rigidbody carRb;
+
+    void Start()
+    {
+        carRb = GetComponent<Rigidbody>();
+    }
+
+    void update()
+    {
+        GetInputs();
+    }
+
+    void LateUpdate()
+    {
+        Move();
+    }
+
+    void GetInputs()
+    {
+        moveInput = Input.GetAxis("Vertical");
+    }
+
+    void Move()
+    {
+        foreach(var wheel in wheels)
+        {
+            wheel.wheelCollider.motorTorque = moveInput * maxAcceleration * Time.deltaTime;
+        }
+    }
+}
