9730427060ae154f82fb4737b5ffb479c09feef8
BramvanOtten <117283572+OpdeBrammert@users.noreply.github.com>
2024-01-17 15:30:23 +0100

    Merge Scenarios met yoram's werk

---
+++ Assets/Scripts/mainMenuScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenuScript : MonoBehaviour
+{
+    // Quit mogelijkheid
+    public void Quit() {
+        Debug.Log("De Speler heeft het spel verlaten");
+    }
+}
