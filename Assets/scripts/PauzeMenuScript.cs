eae2d3407dc76a001ff842990e06e6e25b501aa1
BramvanOtten <117283572+OpdeBrammert@users.noreply.github.com>
2024-01-18 09:51:38 +0100

    update

--- Assets/scripts/PauzeMenuScript.cs
+++ Assets/scripts/PauzeMenuScript.cs
@@ -12,58 +12,6 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         Time.timeScale = 1f;
-        
-    }
-
-    // Update word geroepen per frame
-    void Update()
-    {
-        if(Input.GetKeyDown(KeyCode.Escape))    // If escape ingedrukt word
-        {
-            if (gepauzeerd)                     // Als gepauzeerd, speel game verder af
-            {
-                Verder();
-            }
-            else                                // Anders, game op pauze
-            {
-                Stop();
-            }
-        }
-    }
-
-    void Stop()                                 // Zet de game op pauze
-    {
-        PauzeMenuCanvas.SetActive(true);
-        Time.timeScale = 0f;
-        gepauzeerd = true;
-    }
-
-    void Verder()                               // Laat de game verder gaan
-    {
-        PauzeMenuCanvas.SetActive(false);
-        Time.timeScale = 1f;
-        gepauzeerd = false;
-    }
-
-    public void MainMenuKnop()          // Knop voor MainMenu
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);       // Gaat naar MainMenu scene wanneer je hier op klikt
-    }
-}
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class NewBehaviourScript : MonoBehaviour
-{
-    public static bool gepauzeerd = false;
-    public GameObject PauzeMenuCanvas;       // PauzeMenuCanvas object in de game
-
-    // Start word geroepen voor de eerste frame update
-    void Start()
-    {
-        Time.timeScale = 1f;
     }
 
     // Update word geroepen per frame
