42d67b41c1d426638af49e74d9036810df41c3b9
eren yeager <gdutched@gmail.com>
2024-01-18 16:26:29 +0100

    map changes, navigatie begonnen, en mini map af

---
+++ Assets/scripts/Buttonnavigatie.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttonnavigatie : MonoBehaviour
+{
+    public GameObject Navigatie;
+   public void Continue()
+   {
+        Navigatie.SetActive(false);
+   }
+}
