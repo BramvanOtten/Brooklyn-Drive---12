4cb08ed73f5b8d97a862af90d77a708bdd849b9e
BramvanOtten <117283572+OpdeBrammert@users.noreply.github.com>
2024-01-19 13:28:39 +0100

    Menu scenes aangepast een verkeersborden en voorrangsregels toegevoegd

--- Assets/scripts/CarController.cs
+++ Assets/scripts/CarController.cs
@@ -20,34 +20,35 @@ public class CarController : MonoBehaviour
     }
     public float maxAcceleration = 90.0f;
     public float breakAcceleration = 50.0f;
-
+ 
     public float turnSensitivity = 1.0f;
     public float maxSteerAngle = 30f;
-
+ 
     public Vector3 _centerOfMass;
-
+ 
     public List<Wheel> wheels;
  
     float moveInput;
     float steerInput;
-
+ 
     private Rigidbody carRb;
  
     void Start()
     {
         carRb = GetComponent<Rigidbody>();
         carRb.centerOfMass = _centerOfMass;
-        carRb.centerOfMass = _centerOfMass;
     }
-
+ 
     void Update()
     {
         GetInputs();
     }
-
+ 
     void LateUpdate()
     {
         Move();
+        Steer();
+        Brake();
     }
  
     void GetInputs()
@@ -55,16 +56,6 @@ public class CarController : MonoBehaviour
         moveInput = Input.GetAxis("Vertical");
         steerInput = Input.GetAxis("Horizontal");
     }
-
-    void Move()
-    {
-        foreach(var wheel in wheels)
-        {
-            wheel.wheelCollider.motorTorque = moveInput * 600 * maxAcceleration * Time.deltaTime;
-        }
-    }
-}
-
  
     void Move()
     {
