837c8e44dc063130f10f41f18f3e28530a0627b5
eren yeager <gdutched@gmail.com>
2024-01-19 12:00:43 +0100

    Update CameraController.cs

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -2,18 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraController : MonoBehaviour
+public class cameraController : MonoBehaviour
 {
     float rotationX = 0f;
     float rotationY = 0f;
 
-    public float sensitvity = 15f;
+    public float sensitivity = 15f;
 
-    // Update is called once per frame
     void Update()
     {
-        rotationY += Input.GetAxis("Mouse X") * sensitvity;
-        rotationX += Input.GetAxis("Mouse Y") * -1 * sensitvity;
+        // zodat je rond kan kijken
+        rotationY += Input.GetAxis("Mouse X") * sensitivity;
+        rotationX += Input.GetAxis("Mouse Y") * -1 * sensitivity;
         transform.localEulerAngles = new Vector3(rotationX, rotationY, 0);
+
+        // zodat je de camera vastzet 
+        rotationX = Mathf.Clamp(rotationX, -90f, 90f);
+        
     }
 }
